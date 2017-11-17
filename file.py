6009f1a96117a23cf485872ee0da9719d73a8c0a
lukewarm20 <lukewillb20@gmail.com>
2017-11-17 02:12:34 +0000

    Add files via upload

---
+++ file.py
@@ -0,0 +1,16 @@
+import os
+import time
+import pyautogui
+
+pyautogui.size()
+(1366, 768)
+
+
+pyautogui.hotkey('ctrl' + 'alt' + 't')
+pyautogui.moveTo(392, 206)
+pyautogui.click(clicks=1)
+pyautogui.moveTo(399, 290)
+pyautogui.click(clicks=1)
+pyautogui.click(button='right')
+pyautogui.moveTo(440, 370)
+pyautogui.click(button='right')
