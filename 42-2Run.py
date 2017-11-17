12e4899eb422c83c606f55df67223e7b7644fce6
lukewarm20 <lukewillb20@gmail.com>
2017-11-17 03:05:56 +0000

    Add files via upload

---
+++ 42-2Run.py
@@ -0,0 +1,128 @@
+import os
+import time
+import pyautogui
+import string
+from Read import getUser, getMessage
+from Socket import openSocket, sendMessage
+from Initialize import joinRoom
+
+s = openSocket()
+joinRoom(s)
+readbuffer = ""
+
+while True:
+		readbuffer = readbuffer + s.recv(1024)
+		temp = string.split(readbuffer, "\n")
+		readbuffer = temp.pop()
+
+		for line in temp:
+			print(line)
+                        user = getUser(line)
+                        message = getMessage(line)
+                        pyautogui.size()
+			(1366, 768)
+                        if "a" in message:
+                                pyautogui.typewrite('a')
+                                break
+                        if "b" in message:
+                                pyautogui.typewrite('b')
+                                break
+                        if "c" in message:
+                                pyautogui.typewrite('c')
+                                break
+                        if "d" in message:
+                                pyautogui.typewrite('d')
+                                break
+                        if "e" in message:
+                                pyautogui.typewrite('e')
+                                break
+                        if "f" in message:
+                                pyautogui.typewrite('f')
+                                break
+                        if "g" in message:
+                                pyautogui.typewrite('g')
+                                break
+                        if "h" in message:
+                                pyautogui.typewrite('h')
+                                break
+                        if "i" in message:
+                                pyautogui.typewrite('i')
+                                break
+                        if "j" in message:
+                                pyautogui.typewrite('j')
+                                break
+                        if "k" in message:
+                                pyautogui.typewrite('k')
+                                break
+                        if "l" in message:
+                                pyautogui.typewrite('l')
+                                break
+                        if "m" in message:
+                                pyautogui.typewrite('m')
+                                break
+                        if "n" in message:
+                                pyautogui.typewrite('n')
+                                break
+                        if "o" in message:
+                                pyautogui.typewrite('o')
+                                break
+                        if "p" in message:
+                                pyautogui.typewrite('p')
+                                break
+                        if "q" in message:
+                                pyautogui.typewrite('q')
+                                break
+                        if "r" in message:
+                                pyautogui.typewrite('r')
+                                break
+                        if "s" in message:
+                                pyautogui.typewrite('s')
+                                break
+                        if "t" in message:
+                                pyautogui.typewrite('t')
+                                break
+                        if "u" in message:
+                                pyautogui.typewrite('u')
+                                break
+                        if "v" in message:
+                                pyautogui.typewrite('v')
+                                break
+                        if "w" in message:
+                                pyautogui.typewrite('w')
+                                break
+                        if "x" in message:
+                                pyautogui.typewrite('x')
+                                break
+                        if "y" in message:
+                                pyautogui.typewrite('y')
+                                break
+                        if "z" in message:
+                                pyautogui.typewrite('z')
+                                break
+                        if "!" in message:
+                                pyautogui.hotkey('enter')
+
+				##keep the terminal in the normal spot
+				##right click the top of the terminal window and click always on top
+				pyautogui.moveTo(392, 206)
+				pyautogui.click(clicks=1)
+				pyautogui.moveTo(399, 290)
+				pyautogui.click(clicks=1)
+				pyautogui.click(button='right')
+				pyautogui.moveTo(440, 370)
+				pyautogui.click(button='right')
+				pyautogui.moveTo(1037, 599)
+				pyautogui.click(button='right')
+				pyautogui.moveTo(1061, 416)
+				pyautogui.click(clicks=1)
+				pyautogui.hotkey('enter')
+				pyautogui.moveTo(634, 379)
+				pyautogui.click(clicks=1)
+
+				break
+
+
+
+				##add extras for capitals, symbols and numbers
+
+
