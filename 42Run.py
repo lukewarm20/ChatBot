45647d02e80f0ac3613b995f43947e979eec053d
lukewarm20 <lukewillb20@gmail.com>
2017-11-17 02:20:08 +0000

    Update 42Run.py

--- 42Run.py
+++ 42Run.py
@@ -99,4 +99,15 @@ while True:
                                 break
                         if "!" in message:
                                 pyautogui.hotkey('enter')
+				pyautogui.sleep('1')
+				pyautogui.size()
+				(1366, 768)
+				
+				pyautogui.moveTo(392, 206)
+				pyautogui.click(clicks=1)
+				pyautogui.moveTo(399, 290)
+				pyautogui.click(clicks=1)
+				pyautogui.click(button='right')
+				pyautogui.moveTo(440, 370)
+				pyautogui.click(button='right')
                                 break
